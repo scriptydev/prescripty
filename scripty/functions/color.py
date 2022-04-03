5a51a9914b428884ae9cfc35729ecbccfcdb10de
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-04-03 11:04:39 -0500

    Add remaining classmethods for Discord colors

--- scripty/functions/color.py
+++ scripty/functions/color.py
@@ -4,5 +4,25 @@ class Color:
         return 0x5865F2
 
     @classmethod
+    def green(cls):
+        return 0x57F287
+
+    @classmethod
+    def yellow(cls):
+        return 0xFEE75C
+
+    @classmethod
+    def fuchsia(cls):
+        return 0xEB459E
+
+    @classmethod
     def red(cls):
         return 0xED4245
+
+    @classmethod
+    def white(cls):
+        return 0xFFFFFF
+
+    @classmethod
+    def black(cls):
+        return 0x000000
