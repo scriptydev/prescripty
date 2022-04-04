cdedd013118608aec0805ac8bcedf94257c9ac05
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-04-04 17:41:24 -0500

    Add check for echo command

--- scripty/plugins/misc.py
+++ scripty/plugins/misc.py
@@ -24,13 +24,17 @@ async def avatar(ctx: lightbulb.Context) -> None:
 
 
 @misc.command()
+@lightbulb.add_checks(
+    lightbulb.has_guild_permissions(hikari.Permissions.MANAGE_MESSAGES)
+)
 @lightbulb.option("text", "Text to repeat", str)
 @lightbulb.command("echo", "Repeats user input", auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
 async def echo(ctx: lightbulb.Context) -> None:
+    text = ctx.options.text
     embed = hikari.Embed(
         title="Echo",
-        description=f"{ctx.author.mention} said: ```{ctx.options.text}```",
+        description=f"{ctx.author.mention} said: ```{text}```",
         color=functions.Color.blurple(),
     )
     await ctx.respond(embed)
