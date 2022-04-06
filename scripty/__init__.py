672bc3bfed9cc17528590f78f36b11a0b01ec864
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-04-05 22:48:35 -0500

    Update Discord discriminator for GoogleGenius

--- scripty/__init__.py
+++ scripty/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Erich Nguyen, Johan Joseph, and Hrishikesh Kondiboyina"
 __github__ = ["GoogleGenius", "johan53", "HrishiK100"]
-__discord__ = ["GoogleGenius#3876", "johann#2022", "hrish#7294"]
+__discord__ = ["GoogleGenius#2007", "johann#2022", "hrish#7294"]
 __copyright__ = "Copyright 2022-Present Scripty Development"
 __credits__ = []
 __license__ = "Apache License Version 2.0"
