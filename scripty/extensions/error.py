18130ec103d3155e32f24eae025e162d6d5c5ca2
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-04-10 17:11:32 -0500

    Update mod commands
    Update checks, error handling, and timeout command

--- scripty/extensions/error.py
+++ scripty/extensions/error.py
@@ -9,10 +9,37 @@ error = lightbulb.Plugin("Error")
 
 @error.listener(lightbulb.CommandErrorEvent)
 async def on_error(event: lightbulb.CommandErrorEvent) -> None:
-    if isinstance(event.exception, lightbulb.CommandInvocationError):
+    exception = event.exception.__cause__ or event.exception
+
+    # if isinstance(exception, lightbulb.CommandInvocationError):
+    #     embed = hikari.Embed(
+    #         title="Error",
+    #         description="This interaction failed!",
+    #         color=functions.Color.red(),
+    #     )
+    #     await event.context.respond(embed)
+    #     raise event.exception
+
+    if isinstance(exception, lightbulb.BotMissingRequiredPermission):
         embed = hikari.Embed(
             title="Error",
-            description="This interaction failed!",
+            description=f"Bot missing required `{exception.missing_perms}` permission!",
+            color=functions.Color.red(),
+        )
+        await event.context.respond(embed)
+
+    elif isinstance(exception, lightbulb.MissingRequiredPermission):
+        embed = hikari.Embed(
+            title="Delete Error",
+            description=f"User missing required `{exception.missing_perms}` permission!",
+            color=functions.Color.red(),
+        )
+        await event.context.respond(embed)
+
+    else:
+        embed = hikari.Embed(
+            title="Error",
+            description=f"This interaction failed! \n```{exception}```",
             color=functions.Color.red(),
         )
         await event.context.respond(embed)
