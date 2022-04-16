b8fee80aa3e7b277ccea34552cac99a4e2c8bb61
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-04-16 15:48:14 -0500

    Revert guild ids back to list

--- scripty/constants/config.py
+++ scripty/constants/config.py
@@ -9,9 +9,9 @@ import hikari
 load_dotenv()
 
 DEV_ID: hikari.Snowflake = hikari.Snowflake(os.environ["GUILD_ID_PRIMARY"])
-GUILD_IDS: tuple[hikari.Snowflake, ...] = (
+GUILD_IDS: list[hikari.Snowflake] = [
     hikari.Snowflake(os.environ["GUILD_ID_PRIMARY"]),
     hikari.Snowflake(os.environ["GUILD_ID_SECONDARY"]),
-)
+]
 INVITE_URL: str = os.environ["INVITE_URL"]
 TOKEN: str = os.environ["TOKEN"]
