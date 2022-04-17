4aebbb0abbd545de354a350ef497cd3c0219edfd
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-04-16 21:15:50 -0500

    Move commands to info group

--- scripty/extensions/util.py
+++ scripty/extensions/util.py
@@ -12,10 +12,17 @@ util = lightbulb.Plugin("Utility")
 
 
 @util.command
+@lightbulb.command("info", "Get bot statistics", auto_defer=True)
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def info() -> None:
+    pass
+
+
+@info.child
 @lightbulb.command(
     "about", "About the Scripty Discord bot", auto_defer=True, ephemeral=True
 )
-@lightbulb.implements(lightbulb.SlashCommand)
+@lightbulb.implements(lightbulb.SlashSubCommand)
 async def about(ctx: lightbulb.Context) -> None:
     app_user = ctx.app.get_me()
     assert app_user is not None, "App must be started"
@@ -39,9 +46,47 @@ async def about(ctx: lightbulb.Context) -> None:
     await ctx.respond(embed)
 
 
-@util.command
+class InviteView(miru.View):
+    def __init__(self) -> None:
+        super().__init__()
+        self.add_item(
+            miru.Button(
+                label="Add to Server",
+                url=scripty.constants.INVITE_URL,
+            )
+        )
+
+
+@info.child
+@lightbulb.command("invite", "Add bot to server", auto_defer=True, ephemeral=True)
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def invite(ctx: lightbulb.Context) -> None:
+    view = InviteView()
+
+    embed = hikari.Embed(
+        title="Invite",
+        description="Invite Scripty to your Discord Server!",
+        color=scripty.functions.Color.background_secondary(),
+    )
+
+    await ctx.respond(embed, components=view.build())
+
+
+@info.child
+@lightbulb.command("ping", "Replies with bot latency", auto_defer=True, ephemeral=True)
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def ping(ctx: lightbulb.Context) -> None:
+    embed = hikari.Embed(
+        title="Ping",
+        description=f"Pong! `{round(ctx.app.heartbeat_latency * 1000)}ms`",
+        color=scripty.functions.Color.background_secondary(),
+    )
+    await ctx.respond(embed)
+
+
+@info.child
 @lightbulb.command("system", "Bot system information", auto_defer=True, ephemeral=True)
-@lightbulb.implements(lightbulb.SlashCommand)
+@lightbulb.implements(lightbulb.SlashSubCommand)
 async def system(ctx: lightbulb.Context) -> None:
     system = platform.uname()
 
@@ -74,47 +119,9 @@ async def system(ctx: lightbulb.Context) -> None:
     await ctx.respond(embed)
 
 
-class InviteView(miru.View):
-    def __init__(self) -> None:
-        super().__init__()
-        self.add_item(
-            miru.Button(
-                label="Add to Server",
-                url=scripty.constants.INVITE_URL,
-            )
-        )
-
-
-@util.command
-@lightbulb.command("invite", "Add bot to server", auto_defer=True, ephemeral=True)
-@lightbulb.implements(lightbulb.SlashCommand)
-async def invite(ctx: lightbulb.Context) -> None:
-    view = InviteView()
-
-    embed = hikari.Embed(
-        title="Invite",
-        description="Invite Scripty to your Discord Server!",
-        color=scripty.functions.Color.background_secondary(),
-    )
-
-    await ctx.respond(embed, components=view.build())
-
-
-@util.command
-@lightbulb.command("ping", "Replies with bot latency", auto_defer=True, ephemeral=True)
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx: lightbulb.Context) -> None:
-    embed = hikari.Embed(
-        title="Ping",
-        description=f"Pong! `{round(ctx.app.heartbeat_latency * 1000)}ms`",
-        color=scripty.functions.Color.background_secondary(),
-    )
-    await ctx.respond(embed)
-
-
-@util.command
+@info.child
 @lightbulb.command("uptime", "Replies with bot uptime", auto_defer=True, ephemeral=True)
-@lightbulb.implements(lightbulb.SlashCommand)
+@lightbulb.implements(lightbulb.SlashSubCommand)
 async def uptime(ctx: lightbulb.Context) -> None:
     uptime_resolved_full = f"<t:{ctx.app.d.uptime}:F>"
     uptime_resolved_relative = f"<t:{ctx.app.d.uptime}:R>"
