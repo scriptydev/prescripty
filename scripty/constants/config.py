2bcca6136fb877281eac318cfd71e3a4c1f85e64
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-04-25 13:27:37 -0500

    Change to `typing.Final` for consts

--- scripty/constants/config.py
+++ scripty/constants/config.py
@@ -1,3 +1,5 @@
+import typing
+
 import toml
 
 
@@ -10,7 +12,7 @@ __all__: list[str] = [
 
 config = toml.load("config.toml")
 
-DISCORD_TOKEN: str = config["discord_token"]
-GUILD_ID_PRIMARY: int = config["guild_id_primary"]
-GUILD_ID_SECONDARY: int = config["guild_id_secondary"]
-INVITE_URL: str = config["invite_url"]
+DISCORD_TOKEN: typing.Final[str] = config["discord_token"]
+GUILD_ID_PRIMARY: typing.Final[int] = config["guild_id_primary"]
+GUILD_ID_SECONDARY: typing.Final[int] = config["guild_id_secondary"]
+INVITE_URL: typing.Final[str] = config["invite_url"]
