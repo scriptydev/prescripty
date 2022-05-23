8894f6c43b1b0a8de72b29b3a3992212fb86665b
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-05-23 10:29:44 -0500

    Reorder dunder metadata

--- scripty/__init__.py
+++ scripty/__init__.py
@@ -16,8 +16,8 @@ from .functions import *
 
 __author__: Final[str] = "Erich Nguyen"
 __github__: Final[str] = "GoogleGenius"
-__repository__: Final[str] = "https://github.com/scriptydev/prescripty"
 __discord__: Final[str] = "GoogleGenius#7777"
+__repository__: Final[str] = "https://github.com/scriptydev/prescripty"
 __copyright__: Final[str] = "Copyright 2022-Present Scripty Development"
 __license__: Final[str] = "Apache License Version 2.0"
 __version__: Final[str] = "1.0.0a6"
