155102fe116b0f7c8aa7e566b170967d8fd8c558
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-05-29 12:58:40 -0500

    Document magic methods

--- scripty/functions/cache.py
+++ scripty/functions/cache.py
@@ -8,7 +8,7 @@ from typing import Any
 
 # https://gist.github.com/davesteele/44793cd0348f59f8fadd49d7799bd306
 class LRUCacheDict(OrderedDict[Any, Any]):
-    """Dict with a limited length, ejecting LRUs as needed"""
+    """Dict with a limited length, ejecting LRUs as needed."""
 
     def __init__(self, *args: Any, cache_len: int, **kwargs: Any):
         self.cache_len = cache_len
@@ -16,6 +16,7 @@ class LRUCacheDict(OrderedDict[Any, Any]):
         super().__init__(*args, **kwargs)
 
     def __setitem__(self, key: Any, value: Any):
+        """Set an item in the cache dict and move to end."""
         super().__setitem__(key, value)
         super().move_to_end(key)
 
@@ -24,6 +25,7 @@ class LRUCacheDict(OrderedDict[Any, Any]):
             super().__delitem__(old_key)
 
     def __getitem__(self, key: Any):
+        """Get an item from the cache dict and move to end."""
         val = super().__getitem__(key)
         super().move_to_end(key)
 
