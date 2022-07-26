04d7efd32fa61e03bb8fc01a56103c6aa63a67ac
GoogleGenius <81032623+GoogleGenius@users.noreply.github.com>
2022-07-26 14:40:30 -0500

    Change poll options to numbers
    Because @Kushal-vanavasam said he wouldn't use Scripty unless this was changed >:(

--- scripty/modules/misc.py
+++ scripty/modules/misc.py
@@ -124,16 +124,16 @@ async def echo(ctx: tanjun.abc.SlashContext, text: str) -> None:
 async def poll(
     ctx: tanjun.abc.SlashContext,
     topic: str,
-    option_a: str,
-    option_b: str,
-    option_c: str | None = None,
-    option_d: str | None = None,
-    option_e: str | None = None,
-    option_f: str | None = None,
-    option_g: str | None = None,
-    option_h: str | None = None,
-    option_i: str | None = None,
-    option_j: str | None = None,
+    option_1: str,
+    option_2: str,
+    option_3: str | None = None,
+    option_4: str | None = None,
+    option_5: str | None = None,
+    option_6: str | None = None,
+    option_7: str | None = None,
+    option_8: str | None = None,
+    option_9: str | None = None,
+    option_10: str | None = None,
 ) -> None:
     """Create a simple poll
 
@@ -141,38 +141,38 @@ async def poll(
     ----------
     topic : str
         Topic of the poll
-    option_a : str
+    option_1 : str
         Option A
-    option_b : str
+    option_2 : str
         Option B
-    option_c : str | None
+    option_3 : str | None
         Option C
-    option_d : str | None
+    option_4 : str | None
         Option D
-    option_e : str | None
+    option_5 : str | None
         Option E
-    option_f : str | None
+    option_6 : str | None
         Option F
-    option_g : str | None
+    option_7 : str | None
         Option G
-    option_h : str | None
+    option_8 : str | None
         Option H
-    option_i : str | None
+    option_9 : str | None
         Option I
-    option_j : str | None
+    option_10 : str | None
         Option J
     """
     options: dict[str, str | None] = {
-        "\U0001f1e6": option_a,
-        "\U0001f1e7": option_b,
-        "\U0001f1e8": option_c,
-        "\U0001f1e9": option_d,
-        "\U0001f1ea": option_e,
-        "\U0001f1eb": option_f,
-        "\U0001f1ec": option_g,
-        "\U0001f1ed": option_h,
-        "\U0001f1ee": option_i,
-        "\U0001f1ef": option_j,
+        "1️⃣": option_1,
+        "2️⃣": option_2,
+        "3️⃣": option_3,
+        "4️⃣": option_4,
+        "5️⃣": option_5,
+        "6️⃣": option_6,
+        "7️⃣": option_7,
+        "8️⃣": option_8,
+        "9️⃣": option_9,
+        "🔟": option_10,
     }
 
     embed = embeds.Embed(
